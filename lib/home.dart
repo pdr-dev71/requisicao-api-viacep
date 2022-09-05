d121b1c6f8b4cadfad26fe1076e588c0670543c7
pdr-dev71 <joaopedrosr2699@gmail.com>
2022-09-04 22:23:14 -0300

    Retrieving requisitions in the ui

--- lib/home.dart
+++ lib/home.dart
@@ -11,8 +11,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final TextEditingController _controller = TextEditingController();
+
+  String _result = 'Resultado';
   _recoverCep() async {
-    String urlApi = 'https://viacep.com.br/ws/64655000/json/';
+    String cepInput = _controller.text;
+    String urlApi = 'https://viacep.com.br/ws/$cepInput/json/';
     http.Response response;
 
     response = await http.get(Uri.parse(urlApi));
@@ -21,9 +25,15 @@ class _HomeState extends State<Home> {
 
     String cep = responseObj['cep'];
     String logradouro = responseObj['logradouro'];
+    String localidade = responseObj['localidade'];
     String uf = responseObj['uf'];
+    String ddd = responseObj['ddd'];
 
-    print('Os dados são: Cep:$cep, Logradouro: $logradouro e UF: $uf');
+    setState(() {
+      _result = '$cep, $logradouro, $localidade, $uf, $ddd';
+    });
+
+    print('Os dados são: Cep:$cep, Logradouro: $localidade e UF: $uf');
   }
 
   @override
@@ -34,10 +44,37 @@ class _HomeState extends State<Home> {
         ),
         body: Column(
           children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('assets/images/search.png',
+                  width: MediaQuery.of(context).size.width * .6,
+                  height: MediaQuery.of(context).size.height * .2),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextField(
+                controller: _controller,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                    labelText: 'Digite o cep, formato 00000000'),
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
             ElevatedButton(
               onPressed: _recoverCep,
               child: const Text('Recuperar Cep'),
-            )
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(),
+              ),
+              child: Text(_result),
+            ),
           ],
         ));
   }
